c82fedafa641c75ae974f1498e8cac19174e0705
Gali998 <galimannage6.gm@gmail.com>
2021-05-06 23:01:15 +0530

    SignUp page added

---
+++ src/UserSignup.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './CSS/UserSignUp.css';
+
+export default class UserSignup extends React.Component {
+
+
+    render() {
+        return (
+            <div className={"container"}>
+                <div className={"form"}>
+                    <div className={"heading"}>
+                        <h1>Registration form</h1>
+                    </div>
+                    <div className={"wrap"}>
+                        <div className={"f1"}>
+                            <label>First Name</label>
+                            <input type={"text"}/>
+                            <span className={"focus-input"}></span>
+                        </div>
+                        <div className={"f2"}>
+                            <label>Last Name</label>
+                            <input type={"text"}/>
+                            <span className={"focus-input"}></span>
+                        </div>
+                    </div>
+                    <div className={"wrap2"}>
+                        <label>Username</label>
+                        <input type={"text"}/>
+                        <span className={"focus-input2"}></span>
+                    </div>
+                    <div className={"wrap2"}>
+                        <label>E-mail</label>
+                        <input type={"text"}/>
+                        <span className={"focus-input2"}></span>
+                    </div>
+                    <div className={"wrap2"}>
+                        <label>Phone Number</label>
+                        <input type={"number"}/>
+                        <span className={"focus-input2"}></span>
+                    </div>
+                    <button className={"btn"}>Register</button>
+                </div>
+
+                <div className={"image"}>
+                    <img className={"img"} alt={""}/>
+                </div>
+
+
+            </div>
+        )
+
+    }
+};
